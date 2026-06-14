96f2fe039a83d16a930b45a10edf6c9b81ecde04
toikkkk <thoriqhisam05@gmail.com>
2026-06-14 13:05:39 +0700

    Allow any vercel.app domain in CORS middleware using regex to prevent deployment blockages

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,7 @@ if frontend_url_env:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app|http://localhost:\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
